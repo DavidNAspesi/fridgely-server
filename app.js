b5fd1c6f51d3cff392a89705f88cc4dfbe0948ea
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-04-05 17:21:22 -0600

    checking if need to commit

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ app.use(cors())
 app.use('/user', require('./routes/user'))
 
 app.post('/image', function (req, res) {
-  console.log(req.body);
-  res = res.status(201);
-  // if (req.get('Content-Type')) {
-  //   console.log("Content-Type: " + req.get('Content-Type'));
-  //   res = res.type(req.get('Content-Type'));
-  // }
-  res.send(req.body);
-});
+  console.log(req.body)
+
+
+
+  res = res.status(201)
+  res.send(req.body)
+})
 
 // app.use('/image', require('./routes/image'))
 
