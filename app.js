fc377cb5271b9ad0a4c2ceba1dd973a3609b96e5
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-04-06 10:27:00 -0600

    fixed issue where s3 not recognizing bucket name

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const s3 = new aws.S3({
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.AWS_BUCKET,
+    bucket: 'fridgely',
     acl: 'public-read',
     metadata(req, file, cb) {
       cb(null, {fieldName: file.fieldname});
